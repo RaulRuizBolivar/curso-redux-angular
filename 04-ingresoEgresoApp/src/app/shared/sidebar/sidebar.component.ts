254fad6926fa110234b07bbde1e157cbe5b0da1d
Raúl Ruiz Bolivar <raul.ruiz@atmira.com>
2024-04-17 09:28:14 +0200

    nombre usuario en sidebar

--- 04-ingresoEgresoApp/src/app/shared/sidebar/sidebar.component.ts
+++ 04-ingresoEgresoApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { DialogService } from '../../services/dialog.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: ``
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  nombreUsuario ?: string = '';
 
   constructor(
     private authService:AuthService,
     private router: Router,
-    private dialogService: DialogService
+    private dialogService: DialogService,
+    private store: Store<AppState>
   ){}
+
+  ngOnInit(): void {
+    this.store.select('auth').pipe(filter(({user})=> user != null)).subscribe(({user})=>{
+      console.log(user)
+      this.nombreUsuario = user?.nombre
+    })
+  }
 
   logout(){
     this.dialogService.abrirLoading()
